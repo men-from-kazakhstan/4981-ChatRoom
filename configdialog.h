a4c529d0073194ee894ef81512f2f006ce5ce30c
User <yoz@YozCardel.telus>
2017-03-11 17:59:08 -0800

    Added basic windows and UI elements needed for the base case of the chat room assignment

---
+++ configdialog.h
@@ -0,0 +1,48 @@
+// configdialog.h
+
+#ifndef CONFIGDIALOG_H
+#define CONFIGDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class ConfigDialog;
+}
+
+class ConfigDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    /* Qt created functions */
+    explicit ConfigDialog(QWidget *parent = 0); // constructor
+    ~ConfigDialog(); // destructor
+
+private slots:
+    /********************************************************
+     *  Function:   on_cnfgOkButton_clicked()
+     *
+     *  Programmer: Alex Zielinski
+     *
+     *  Created: Mar 11 2017
+     *
+     *  Modified:
+     *
+     *  Desc:
+     *      Event handler for when the OK button is
+     *      clicked within the configuration dialog
+     *      of the Client window. When clicked the
+     *      username, port and IP data provided by
+     *      user is validated. If something is invalid
+     *      then the user is notifyed and the told to
+     *      enter a valid value. Once all user entered
+     *      data is valid then the data gets saved and
+     *      the user is redirected to the Client window
+     *******************************************************/
+    void on_cnfgOkButton_clicked();
+
+private:
+    Ui::ConfigDialog *ui;
+};
+
+#endif // CONFIGDIALOG_H
