efb65dcf65646ed90b96c34d999115b9c395571a
Matt Goerwell <superninja246@hotmail.com>
2017-03-12 15:43:54 -0700

    added the format message state

--- 4981_ChatRoom/client.h
+++ 4981_ChatRoom/client.h
@@ -24,4 +24,22 @@
  *******************************************************/
 void getUserMessage(ClientWindow *cw, char *completeMsg, char *IP, char *username);
 
+/********************************************************
+ *  Function:       void formatMessage(char *message, char *IP, char *username)
+ *                      char *message: buffer that holds message to be formatted.
+ *                      char *IP: string containing the IP of the client sending the message
+ *                      char *username: client username
+ *
+ *  Programmer:     Matt Goerwell
+ *
+ *  Created:        Mar 11 2017
+ *
+ *  Modified:
+ *
+ *  Desc:
+ *      This will take the message passed into and apply standard formatting rules.
+ *      That is to say, It will prepend the message with the timestamp, username, and
+ *      IP address of the sender. It will also colour code the username eventually.
+ *******************************************************/
+char* formatMessage(const char *message, const char *IP, const char *username);
 #endif // CLIENT_H
--- 4981_ChatRoom/clientwindow.cpp
+++ 4981_ChatRoom/clientwindow.cpp
@@ -35,7 +35,9 @@ ClientWindow::~ClientWindow()
 /* clients message send button event handler */
 void ClientWindow::on_cltSendButton_clicked()
 {
-
+    char *x;
+    x = formatMessage("Hello","127.0.0.1","Matt Goerwell");
+    puts(x);
 }
 
 void ClientWindow::getUIMessage(char *pmsg) {
