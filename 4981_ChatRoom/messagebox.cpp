e3356a482da4a92561c03b602c8d431037a45809
Robert Arendac <robertarendac@gmail.com>
2017-03-22 22:24:31 -0700

    spelling mistake

--- 4981_ChatRoom/messagebox.cpp
+++ 4981_ChatRoom/messagebox.cpp
@@ -36,7 +36,7 @@ MessageBox::MessageBox(QWidget *parent) : QTextEdit(parent)
  *  Modified:
  *
  *  Desc:
- *      Overrides the QTextEdit method to emit a single when
+ *      Overrides the QTextEdit method to emit a signal when
  *      the "return" or "enter" key is pressed instead of going
  *      to a newline
  *******************************************************/
